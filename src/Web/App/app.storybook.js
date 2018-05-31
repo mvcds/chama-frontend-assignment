75283d7f27c5c2aec2b162b1e75ccca73c33542c
Marcos Vinícius C. da Silva <marvinc.silva@gmail.com>
2018-05-31 10:03:53 -0300

    isolate app into its own folder

--- src/Web/App/app.storybook.js
+++ src/Web/App/app.storybook.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 
-import App from './App';
+import App from './index';
 
 storiesOf('App', module)
   .add('Default', () => <App />);
