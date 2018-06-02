d7af793f14b26493272a902c63edd4b1bb40e8f4
Marcos Vinícius C. da Silva <marcos.silva@sprinklr.com>
2018-06-02 14:47:07 -0300

    compact code

--- src/Web/Reducers/todoList.js
+++ src/Web/Reducers/todoList.js
@@ -3,27 +3,13 @@ import TodoList from '../../Domain/Objects/TodoList/todoList.factory';
 const DEFAULT = TodoList.WithNonDoneTodos(5);
 
 const REDUCERS = {
-  TOGGLE_TODO: function (state, { todo, isCompleted }) {
-    return state.editTodo(todo, { isCompleted });
-  },
-  TOGGLE_ALL: function (state, { isCompleted }) {
-    return state.toggleAll(isCompleted);
-  },
-  ADD_TODO: function (state, { text }) {
-    return state.addTodo(text);
-  },
-  EDIT_TODO: function (state, { todo, text }) {
-    return state.editTodo(todo, { text });
-  },
-  DELETE_TODO: function (state, { todo }) {
-    return state.deleteTodo(todo);
-  },
-  TOGGLE_SORTER: function (state) {
-    return state.toggleSorter();
-  },
-  CLEAR_COMPLETED_TODOS: function (state) {
-    return state.clearCompleted();
-  }
+  TOGGLE_TODO: (state, { todo, isCompleted }) => state.editTodo(todo, { isCompleted }),
+  TOGGLE_ALL: (state, { isCompleted }) => state.toggleAll(isCompleted),
+  ADD_TODO: (state, { text }) => state.addTodo(text),
+  EDIT_TODO: (state, { todo, text }) => state.editTodo(todo, { text }),
+  DELETE_TODO: (state, { todo }) => state.deleteTodo(todo),
+  TOGGLE_SORTER: (state) => state.toggleSorter(),
+  CLEAR_COMPLETED_TODOS: (state) => state.clearCompleted()
 }
 
 function todoList (state = DEFAULT, { type, payload }) {
