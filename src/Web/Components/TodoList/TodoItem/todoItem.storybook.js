002f8f52a9d184c1e357f9f9a262167a86e438ea
Marcos Vinícius C. da Silva <marcos.silva@sprinklr.com>
2018-06-01 23:37:54 -0300

    add a storybook for TodoItem

---
+++ src/Web/Components/TodoList/TodoItem/todoItem.storybook.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
+import { lorem } from 'faker';
+
+import TodoItem from './index';
+
+const MIN = 1;
+
+const QUANTITY = {
+  min: MIN,
+  max: 5,
+  range: true
+};
+
+const props = {
+  onToggle: action('toogle'),
+  onStartEditing: action('start editing'),
+  onKeyDown: action('key down'),
+  onChange: action('change'),
+  onExitEditing: action('exit editing')
+}
+
+const todoSeed = lorem.words()
+
+storiesOf('Organisms / Todo Item', module)
+  .addDecorator(withKnobs)
+  .addDecorator((story) => <div className="todo-list__item">{story()}</div>)
+  .add('Static', () => {
+    const textTodo = text('To do', todoSeed)
+    const isCompleted = boolean('Completed?', false)
+
+    const todo = {
+      text: textTodo,
+      isCompleted
+    }
+
+    return (
+      <TodoItem
+        {...props}
+        todo={todo}
+      />
+    )
+  })
+  .add('Editable', () => {
+    const editedTodo = text('Edited', todoSeed)
+
+    return (
+      <TodoItem
+        {...props}
+        isEditing
+        editedTodo={editedTodo}
+      />
+    )
+  })
