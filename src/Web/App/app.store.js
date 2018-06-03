e7807082a2ceae762189f914219effc428b6a44a
Marcos Vinícius C. da Silva <marcos.silva@sprinklr.com>
2018-06-03 13:14:40 -0300

    add redux devtools extension

--- src/Web/App/app.store.js
+++ src/Web/App/app.store.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-import rootReducer from '../Reducers'
 
 import App from './app.redux';
-
-const store = createStore(rootReducer);
+import store from './store';
 
 function AppStore () {
   return (
