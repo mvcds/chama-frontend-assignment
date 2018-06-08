0a33cb76f2d8b3091bf72433bca9df279df71ad9
Marcos Vinícius C. da Silva <marcos.silva@sprinklr.com>
2018-06-08 19:21:40 -0300

    add real time

--- src/Infra/ReduxStore/sagas/fetchTodoList.js
+++ src/Infra/ReduxStore/sagas/fetchTodoList.js
@@ -2,17 +2,12 @@ import { take, select, put, call } from 'redux-saga/effects';
 
 function* fetchTodoList () {
   while (true) {
-    yield take('IGNITE');
-    const { firewatcher, firebase: { auth: { uid } } } = yield select();
-
-    if (!uid) continue;
-
-    const ref = yield call(firewatcher.read, `users/${uid}/todos`);
+    const { data: todos } = yield take('@@reactReduxFirebase/SET');
 
     yield put({
       type: 'READ_TODOS',
       payload: {
-        todos: ref.val()
+        todos
       }
     })
   }
