b43f3fcac17029f07bd45d9099b685f1578909ad
Marcos Vinícius C. da Silva <marcos.silva@sprinklr.com>
2018-06-09 16:16:13 -0300

    move related sagas together

--- src/Infra/ReduxStore/sagas/index.js
+++ src/Infra/ReduxStore/sagas/index.js
@@ -1,22 +1,10 @@
 import { all } from 'redux-saga/effects';
 
-import addTodo from './addTodo';
-import fetchTodoList from './fetchTodoList';
-import editTodo from './editTodo';
-import setDueDate from './setDueDate';
-import editList from './editList.js';
+import todos from './todos';
 
 function* sagas() {
   yield all([
-    addTodo(),
-    fetchTodoList(),
-    editTodo('EDIT_TODO', 'text'),
-    editTodo('TOGGLE_TODO', 'isCompleted'),
-    editTodo('DELETE_TODO', 'isDeleted', true),
-    setDueDate(),
-    editList('MOVE_TODO'),
-    editList('TOGGLE_ALL'),
-    editList('CLEAR_COMPLETED_TODOS')
+    todos()
   ]);
 }
 
---
+++ src/Infra/ReduxStore/sagas/todos/index.js
@@ -0,0 +1,23 @@
+import { all } from 'redux-saga/effects';
+
+import addTodo from './addTodo';
+import fetchTodoList from './fetchTodoList';
+import editTodo from './editTodo';
+import setDueDate from './setDueDate';
+import editList from './editList.js';
+
+function* todos() {
+  yield all([
+    addTodo(),
+    fetchTodoList(),
+    editTodo('EDIT_TODO', 'text'),
+    editTodo('TOGGLE_TODO', 'isCompleted'),
+    editTodo('DELETE_TODO', 'isDeleted', true),
+    setDueDate(),
+    editList('MOVE_TODO'),
+    editList('TOGGLE_ALL'),
+    editList('CLEAR_COMPLETED_TODOS')
+  ]);
+}
+
+export default todos;
